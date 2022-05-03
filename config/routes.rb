6052e45c90a661b53fdee6d2b5f2bad10d1ad2d3
Raif <raif.si@hotmail.com>
2022-05-03 17:12:11 -0300

    CSS movido para ../stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    root to: 'home#index'
-    get '/', to: 'home#index'
-    resources :warehouses
-  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  resources :warehouses
+end
