acde3c9c6a56b128df7a96bbe35a787524e04e42
Raif <raif.si@hotmail.com>
2022-05-03 22:57:37 -0300

    Cadastro de galpoes concluido - video 14

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,8 +1,20 @@
 class WarehousesController < ApplicationController
 
-def show
-  id = params[:id]
-  @warehouse = Warehouse.find(id)
-end
+  def show
+    id = params[:id]
+    @warehouse = Warehouse.find(id)
+  end
+
+  def new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description,
+                                                         :address, :zip_code, :area)
+    w = Warehouse.new(warehouse_params)
+    w.save()
+
+    redirect_to root_path
+  end
 
 end
---
+++ spec/system/user_create_warehouse_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'Usuario cadastra um novo galpao' do
+  it 'a partir da tela inicial' do
+
+    visit root_path
+    click_on 'Cadastrar Galpao'
+
+    expect(page).to have_field('Nome')
+    expect(page).to have_field('Descricao')
+    expect(page).to have_field('Codigo')
+    expect(page).to have_field('Endereco')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Area')
+  end
+
+  it 'com sucesso' do
+    visit root_path
+    click_on 'Cadastrar Galpao'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Descricao', with: 'Galpao do RJ'
+    fill_in 'Codigo', with: 'RIO'
+    fill_in 'Endereco', with: 'Avenida Floriano Peixoto, 1000'
+    fill_in 'Cidade', with: 'Rio de Janeiro'
+    fill_in 'CEP', with: '20200-000'
+    fill_in 'Area', with: '35000'
+    click_on 'Enviar'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Rio de Janeiro'
+    expect(page).to have_content 'RIO'
+    expect(page).to have_content '35000 m2'
+  end
+end
